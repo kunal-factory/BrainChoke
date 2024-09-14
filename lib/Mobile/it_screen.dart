5def476a4f5e6a8f78c8ffda61208d01bc29c28a
kunal <kunal.darje19@gmail.com>
2024-09-14 21:30:36 +0530

    new changes for adding two tabs iT and Lt

--- lib/Mobile/it_screen.dart
+++ lib/Mobile/it_screen.dart
@@ -14,65 +14,76 @@ class ItScreenState extends State<ItScreen> {
       child: Material(
         color: Colors.white,
         child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: ListView.builder(
-              itemCount: 10,
-              padding: const EdgeInsets.all(10),
-              itemBuilder: (context, i) {
-                return i % 2 == 0
-                    ? Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: const Color.fromARGB(255, 80, 101, 218),
-                            borderRadius: BorderRadius.circular(8),
-                            boxShadow: [
-                              BoxShadow(
-                                blurRadius: 5,
-                                color: const Color.fromARGB(255, 66, 66, 69)
-                                    .withOpacity(0.5),
-                                offset: const Offset(2, 5),
-                                spreadRadius: 2,
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Text(
+                  "12 Sept 2024",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+                Expanded(
+                    child: ListView.builder(
+                  itemCount: 10,
+                  padding: EdgeInsets.all(10),
+                  itemBuilder: (context, i) {
+                    return i % 2 == 0
+                        ? Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color.fromARGB(255, 80, 101, 218),
+                                borderRadius: BorderRadius.circular(8),
+                                boxShadow: [
+                                  BoxShadow(
+                                    blurRadius: 5,
+                                    color: const Color.fromARGB(255, 66, 66, 69)
+                                        .withOpacity(0.5),
+                                    offset: const Offset(2, 5),
+                                    spreadRadius: 2,
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                          child: const Padding(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 10, horizontal: 10),
-                            child: Text(
-                              'Hey this is my long text appbar title',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 21,
-                                  fontWeight: FontWeight.w400),
+                              child: const Padding(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 10),
+                                child: Text(
+                                  'Hey this is my long text appbar title',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 21,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ),
                             ),
-                          ),
-                        ),
-                      )
-                    : Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 251, 251, 253),
-                            borderRadius: BorderRadius.circular(8),
-                            border: Border.all(
-                              color: Colors.red, // red as border color
+                          )
+                        : Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color.fromARGB(255, 251, 251, 253),
+                                borderRadius: BorderRadius.circular(8),
+                                border: Border.all(
+                                  color: Colors.red, // red as border color
+                                ),
+                              ),
+                              child: const Padding(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 10),
+                                child: Text(
+                                  'Hey this is my long text appbar title',
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 11, 11, 11),
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ),
                             ),
-                          ),
-                          child: const Padding(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 10, horizontal: 10),
-                            child: Text(
-                              'Hey this is my long text appbar title',
-                              style: TextStyle(
-                                  color: Color.fromARGB(255, 11, 11, 11),
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w400),
-                            ),
-                          ),
-                        ),
-                      );
-              },
+                          );
+                  },
+                ))
+              ],
             )),
       ),
     );
--- lib/Mobile/landing_screen.dart
+++ lib/Mobile/landing_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:projectorders/Mobile/it_screen.dart';
+import 'package:projectorders/Mobile/lt_screen.dart';
 
 class LandingScreen extends StatefulWidget {
   const LandingScreen({super.key});
@@ -23,40 +24,21 @@ class LandingScreenState extends State<LandingScreen> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Drawer Header'),
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Tasks to do!!'),
+            bottom: const TabBar(
+              tabs: [
+                Tab(text: 'Home'),
+                Tab(text: 'Favorites'),
+              ],
             ),
-            ListTile(
-              leading: const Icon(
-                Icons.home,
-              ),
-              title: const Text('Page 1'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(
-                Icons.train,
-              ),
-              title: const Text('Page 2'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
-      body: const ItScreen(),
-    );
+          ),
+          body: const TabBarView(
+            children: [ItScreen(), LtScreen()],
+          ),
+        ));
   }
 }
